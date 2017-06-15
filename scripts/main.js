779ed297e8a7bafbdb627fb4d408752435152881
Jason Tuttle <jason@hearspotbark.com>
2017-06-15 16:59:20 -0400

    functioning data query

--- scripts/main.js
+++ scripts/main.js
@@ -29,20 +29,41 @@
     let searchURL = baseURL + '/tracks?client_id=' + CLIENT_ID;
     const searchObj = {};
     searchObj.q = term;
-    searchObj.limit = 100;
+    searchObj.limit = 25;
     console.log(searchURL, searchObj);
     SC.get('/tracks', searchObj)
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-      })
+      .then(response => processResponse(response)
+    );
+
   }
 
   // 4. Create a way to append the fetch results to your page
-
+  //artwork_url:
+  //stream_url:
+  //user.username:
+  //title:
+  function processResponse(tracks) {
+    console.log(tracks);
+    tracks.forEach(function(track) {
+      console.log("I'm building a trackbox...");
+      let trackBox = document.createElement('div');
+      trackBox.classList.add('track');
+      let trackImg = document.createElement('img');
+      if (track.artwork_url) {
+        trackImg.setAttribute('src', track.artwork_url);
+      } else {
+        trackImg.setAttribute('src', 'images/record_blank.jpeg');
+      }
+      let trackArtist = document.createElement('p');
+      trackArtist.textContent = track.user.username;
+      let trackTitle = document.createElement('p');
+      trackTitle.textContent = track.title;
+      trackBox.appendChild(trackImg);
+      trackBox.appendChild(trackTitle);
+      trackBox.appendChild(trackArtist);
+      results.appendChild(trackBox);
+    });
+  }
 
   // 5. Create a way to listen for a click that will play the song in the audio play
 
