a7a1cd2c334856d9fba23f737ef8a7dcbc370934
Jason Tuttle <jason@hearspotbark.com>
2017-06-15 15:52:05 -0400

    getting started

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# BOILERPLATE README
-- add content here
+# SoundCloud Music Search
+
+SoundCloud keys:
+- 8538a1744a7fdaa59981232897501e04
+- 095fe1dcd09eb3d0e1d3d89c76f5618f
+
+```javascript
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'buskers'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+```
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,53 @@
-/*
-  Here is a guide for the steps you could take:
-*/
+(function() {
+  const CLIENT_ID = "8538a1744a7fdaa59981232897501e04";
+  const CLIENT_ID2 = "095fe1dcd09eb3d0e1d3d89c76f5618f";
+  const baseURL = 'https://api.soundcloud.com';
+  // initialize SoundCloud API with our client ID
+  SC.initialize({
+    client_id: CLIENT_ID
+  });
+  /*
+    Here is a guide for the steps you could take:
+  */
 
-// 1. First select and store the elements you'll be working with
+  // 1. First select and store the elements you'll be working with
+  const musicPlayer = document.querySelector('.music-player');
+  const searchText = document.querySelector('#search-text');
+  const doSearch = document.querySelector('#search');
+  const results = document.querySelector('.results');
 
 
-// 2. Create your `onSubmit` event for getting the user's search term
+  // 2. Create your `onSubmit` event for getting the user's search term
+  doSearch.addEventListener("click", function() {
+    event.preventDefault();
+    performSearch(searchText.value);
+  });
+
+  // 3. Create your `fetch` request that is called after a submission
+  function performSearch() {
+    const term = searchText.value;
+    let searchURL = baseURL + '/tracks?client_id=' + CLIENT_ID;
+    const searchObj = {};
+    searchObj.q = term;
+    searchObj.limit = 100;
+    console.log(searchURL, searchObj);
+    SC.get('/tracks', searchObj)
+      .then(function(response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      })
+  }
+
+  // 4. Create a way to append the fetch results to your page
 
 
-// 3. Create your `fetch` request that is called after a submission
+  // 5. Create a way to listen for a click that will play the song in the audio play
 
 
-// 4. Create a way to append the fetch results to your page
 
 
-// 5. Create a way to listen for a click that will play the song in the audio play
+
+})();
